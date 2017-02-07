5998a30a43422393e4fa48ded3928615ea94f363
Satyajit Ghana <satyajitghana7@gmail.com>
2017-02-07 23:17:04 +0530

    Initial Commit

---
+++ PeriodicTable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PeriodicTable
+//
+//  Created by Satyajit Ghana on 07/02/17.
+//  Copyright © 2017 Satyajit Ghana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
